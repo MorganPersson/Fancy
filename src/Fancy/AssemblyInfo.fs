bd38a89e8794477a86bb4b14c0fc16907402341f
Morgan Persson <lazydev@gmail.com>
2013-12-03 22:52:19 +0100

    assemblyinfo

---
+++ src/Fancy/AssemblyInfo.fs
@@ -0,0 +1,16 @@
+namespace System
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("Fancy")>]
+[<assembly: AssemblyProduct("Fancy")>]
+[<assembly: AssemblyCompany("Fancy")>]
+[<assembly: AssemblyDescription("Fancy - Nancy in F#")>]
+[<assembly: AssemblyCopyright("Copyright (C) Morgan Persson")>]
+[<assembly: AssemblyVersion("0.0.1.0")>]
+[<assembly: AssemblyFileVersion("0.0.1.0")>]
+[<assembly: AssemblyInformationalVersion("0.0.1-local000")>]
+
+[<assembly: Guid("3C8F8DE9-4DD7-46F4-BC0D-25BD9FDA6BE2")>]
+()
